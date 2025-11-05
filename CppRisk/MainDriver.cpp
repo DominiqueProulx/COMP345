e7fa2fb1d441a7ae56868d19951af9745036c64a
Dominique Proulx <dominique.proulx@gmail.com>
2025-11-04 21:43:59 -0500

    restore mapdriver and maindrvier

--- CppRisk/MainDriver.cpp
+++ CppRisk/MainDriver.cpp
@@ -1,35 +1,45 @@
 #include "Map.h"
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
+#include "Player.h"
+#include "Orders.h"
+#include "Cards.h"
+#include "GameEngine.h"
 
-void testLoadMaps() {
-	MapLoader loader;
-	vector<string> paths = {
-		"MAP.txt",
-		"MAP2.txt",
-		"test.txt"
-	};
+// Declare test functions
+void testLoadMaps();
+void testPlayers();
+void testOrdersLists();
+void testCards();
+void testGameStates();
 
-	cout << "========================================" << endl;
-	cout << "      TESTING MAPS AND MAPLOADER" << endl;
-	cout << "========================================" << endl << endl;
+std::string buffer{};
 
-	for (const auto& p : paths) {
-		string err;
-		Map* m = loader.load(p, &err);
-		if (m) {
-			cout << "[OK] Loaded: " << p << "\n";
-			cout << *m << "\n";
-			cout << "  -> validate(): " << (m->validate() ? "SUCCESS" : "FAILURE") << "\n";
-			delete m;
-		}
-		else {
-			cout << "[ERR] " << p << " => " << err << "\n";
-		}
-		cout << "------------------------------------------\n";
-	}
+int main(void)
+{
+	// run all driver test functions
+	testLoadMaps();
+	std::cout << "\n\nFinished testing MAPS. Enter any character to proceed to the next test. ";
+	std::cin >> buffer;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-	cout << endl;
+	testPlayers();
+	std::cout << "\n\nFinished testing PLAYERS. Enter any character to proceed to the next test. ";
+	std::cin >> buffer;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	testOrdersLists();
+	std::cout << "\n\nFinished testing ORDER LISTS. Enter any character to proceed to the next test. ";
+	std::cin >> buffer;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	testCards();
+	std::cout << "\n\nFinished testing CARDS. Enter any character to proceed to the next test. ";
+	std::cin >> buffer;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	testGameStates();
+	std::cout << "\n\nFinished testing GAME ENGINE. Enter any character to end the program. ";
+	std::cin >> buffer;
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	return 0;
 }
